81e45c0424dac4fde54fdeb8b77677181ac2125d
Vitor Salek <vitorsalek@hotmail.com>
2023-12-28 11:31:02 -0300

    testing islands

--- islands/Editor.tsx
+++ islands/Editor.tsx
@@ -1,5 +1,7 @@
 import type { Image as DecoImage } from "deco-sites/std/components/types.ts";
 import { tableMapData } from "../static/MockedTableObject.tsx";
+import { useSignal } from "@preact/signals";
+import { useState } from "preact/hooks";
 
 export interface Props {
   title?: string;
@@ -8,7 +10,9 @@ export interface Props {
 export default function Editor({
   title = "Editor",
 }: Props) {
-  
+  const countSignal = useSignal(0);
+  const [countState, setcountState] = useState(0);
+
   return (
     <div>
       <header class="lg:container mx-auto md:mx-16 lg:mx-auto mt-8 md:mt-12 mb-28 text-xl md:text-base flex flex-col items-center justify-center">
@@ -20,21 +24,50 @@ export default function Editor({
       </header>
 
       <div>
-        <h1>*Table Map*</h1>
         {tableMapData.tables.map((table) => (
           <div key={table.id}>
-            <h2>Table: {table.x}</h2>
+            <h2
+              style={`position: absolute; left: ${table.x}px; top: ${
+                table.y - 30
+              }px;`}
+            >
+              Table: {table.label}
+            </h2>
 
             <img
               src="/tables/greenTable.png"
               alt={`Table ${table.label}`}
               style={`position: absolute; left: ${table.x}px; top: ${table.y}px; transform: rotate(-${table.rotation}deg);`}
             />
-            <div> 
+            {
+              /*<div>
               {table.places.map((place) => <p key={place.id}>{place.label}</p>)}
-            </div>
+        </div>*/
+            }
           </div>
         ))}
+      </div>
+
+      {/*Signal*/}
+      <div  class="p-2">
+        <button onClick={() => countSignal.value--}>
+          -
+        </button>
+        <span class="p-1">Signal: {countSignal}</span>
+        <button onClick={() => countSignal.value++}>
+          +
+        </button>
+      </div>
+
+      {/*useState*/}
+      <div  class="p-2">
+        <button onClick={() => setcountState(countState - 1)}>
+          -
+        </button>
+        <span class="p-1">useState: {countState}</span>
+        <button onClick={() => setcountState(countState + 1)}>
+          +
+        </button>
       </div>
     </div>
   );
